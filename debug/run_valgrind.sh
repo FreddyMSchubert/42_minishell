908bb779510ad32e48147f763d6b0eeb96f53b33
Frederick Schubert <fschuber@1-A-12.42heilbronn.de>
2024-03-18 06:42:37 +0100

    v0.23.5 - functional readline suppression setup

--- debug/run_valgrind.sh
+++ debug/run_valgrind.sh
@@ -1,1 +1,1 @@
-make comp && valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --suppressions=./debug/ignore_readline.supp -s ./crash
+make comp && valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --suppressions=./debug/readline.supp --gen-suppressions=all --log-file=./debug/log.txt -s ./crash
