028c17802293077905ea6aa9931c757f88d84162
Niklas Burchhardt <nburchha@2-G-7.42heilbronn.de>
2024-04-17 14:44:06 +0200

    fixed some return codes of export builtin. 2426/322, stdout:52

--- src/4-executing/builtins/export.c
+++ src/4-executing/builtins/export.c
@@ -6,32 +6,30 @@
 /*   By: nburchha <nburchha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 09:38:36 by fschuber          #+#    #+#             */
-/*   Updated: 2024/04/17 13:14:09 by nburchha         ###   ########.fr       */
+/*   Updated: 2024/04/17 14:17:23 by nburchha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
 
-static int	export_error_return(int error_code, char *arg)
+static int	export_error_return(int error_code, char *arg, t_program_data *data)
 {
 	ft_putstr_fd("export: ", STDERR_FILENO);
 	if (arg)
 		ft_putstr_fd(arg, STDERR_FILENO);
-	if (error_code == 1)
-		ft_putstr_fd(": not enough arguments\n", STDERR_FILENO);
-	else if (error_code == 2)
+	if (error_code == 2)
 		ft_putstr_fd(": not a valid identifier\n", STDERR_FILENO);
 	else if (error_code == 3)
 		ft_putstr_fd(": error setting env variable\n", STDERR_FILENO);
 	else if (error_code == 4)
 		ft_putstr_fd(": error allocating memory\n", STDERR_FILENO);
+	data->exit_status = 1;
 	return (1);
 }
 
 int	execute_export(t_token **tokens, t_program_data *program_data)
 {
 	int		i;
-	int		equ_pos;
 	char	*equ_pos_char;
 	char	*var;
 	char	*value;
@@ -40,24 +38,25 @@ int	execute_export(t_token **tokens, t_program_data *program_data)
 	while (tokens[++i])
 	{
 		equ_pos_char = ft_strchr(tokens[i]->value, '=');
-		if (equ_pos_char != NULL && equ_pos_char > tokens[i]->value)
-			equ_pos = equ_pos_char - tokens[i]->value;
-		else
+		if (!equ_pos_char || equ_pos_char == tokens[i]->value)
 		{
-			if (equ_pos_char == NULL)
-				continue;
-			else
-				return (export_error_return(2, tokens[i]->value));
+			if (equ_pos_char == tokens[i]->value || ft_isnbr(tokens[i]->value))
+				export_error_return(2, tokens[i]->value, program_data);
+			continue;
 		}
-		var = ft_substr(tokens[i]->value, 0, equ_pos);
+		var = ft_substr(tokens[i]->value, 0, equ_pos_char - tokens[i]->value);
 		if (!var)
-			return (export_error_return(4, NULL));
-		value = ft_substr(tokens[i]->value, equ_pos + 1, \
-							ft_strlen(tokens[i]->value) - equ_pos - 1);
+			return (export_error_return(4, NULL, program_data));
+		if (ft_isnbr(var))
+		{
+			export_error_return(2, tokens[i]->value, program_data);
+			continue;
+		}
+		value = ft_substr(tokens[i]->value, equ_pos_char - tokens[i]->value + 1, ft_strlen(tokens[i]->value) - (equ_pos_char - tokens[i]->value) - 1);
 		if (!value)
-			return (export_error_return(4, NULL));
+			return (export_error_return(4, NULL, program_data));
 		if (set_envcp_var(var, value, 1, program_data) != 0)
-			return (export_error_return(3, NULL));
+			return (export_error_return(3, NULL, program_data));
 	}
-	return (0);
+	return (program_data->exit_status);
 }
